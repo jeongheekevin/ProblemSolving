d5fa5c6648cf5a97746ccc0a9906c0d8ad366564
jeongheekevin <45312265+jeongheekevin@users.noreply.github.com>
2022-08-20 00:48:36 +0900

    Update lesson4_2_PermCheck.java

--- Codility/lesson4_CountingElements/lesson4_2_PermCheck.java
+++ Codility/lesson4_CountingElements/lesson4_2_PermCheck.java
@@ -1,30 +1,29 @@
-package lesson4_CountingElements;
-//import java.util.HashSet;
-//
-//public class lesson4_2 {
-//	class Solution {
-//		public int solution(int[] A) {
-//			// write your code in Java SE 8
-//
-//			// N is an integer within the range [1..100,000];
-//			// each element of array A is an integer within the range [1..1,000,000,000].
-//			// permutation is a sequence containing each element from 1 to N once,
-//			// returns 1 if array A is a permutation and 0 if it is not.
-//			HashSet<Integer> hs = new HashSet<>();
-//			int N = A.length;
-//
-//			for (int i = 0; i < N; ++i) {
-//				hs.add(A[i]);
-//			}
-//
-//			for (int i = 1; i <= N; ++i) {
-//				if (!hs.contains(i)) {
-//					return 0;
-//				}
-//			}
-//
-//			return 1;
-//
-//		}
-//	}
-//}
+import java.util.HashSet;
+
+public class lesson4_2 {
+	class Solution {
+		public int solution(int[] A) {
+			// write your code in Java SE 8
+
+			// N is an integer within the range [1..100,000];
+			// each element of array A is an integer within the range [1..1,000,000,000].
+			// permutation is a sequence containing each element from 1 to N once,
+			// returns 1 if array A is a permutation and 0 if it is not.
+			HashSet<Integer> hs = new HashSet<>();
+			int N = A.length;
+
+			for (int i = 0; i < N; ++i) {
+				hs.add(A[i]);
+			}
+
+			for (int i = 1; i <= N; ++i) {
+				if (!hs.contains(i)) {
+					return 0;
+				}
+			}
+
+			return 1;
+
+		}
+	}
+}
